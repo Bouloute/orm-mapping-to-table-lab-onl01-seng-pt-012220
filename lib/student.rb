225205a42c8e38194ff231f6b0189b2a5c07954a
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-22 18:46:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-        id INT PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         name TEXT,
         grade INT
       );
