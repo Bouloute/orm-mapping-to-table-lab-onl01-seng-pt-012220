f425f94edb39ab0fe69dcfafef9b44b0174a668e
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-22 17:56:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,14 +12,14 @@ class Student
   end
 
   def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-        name TEXT,
-        grade INT
-      );
-      SQL
-
-    DB[:conn].execute(sql)
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end
 
 end
